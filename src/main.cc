1325576bcfea6b05f73e2d141637e485ea50cd97
Victor GALLET <vic03@live.fr>
2018-03-12 17:11:22 +0100

    Add failed build to test travis

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(int argc, char** argv){
+
     std::cout << "test" << std::endl;
+    int a = 2;
+    if(a == 2){
+        return EXIT_FAILURE;
+    }
+
     return 0;
 }
