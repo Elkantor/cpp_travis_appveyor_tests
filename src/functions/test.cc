e02cf3a07d1ca8d3f7b2860842a48121fd847354
Victor GALLET <vic03@live.fr>
2018-03-13 10:19:47 +0100

    Add a simple method to test

---
+++ src/functions/test.cc
@@ -0,0 +1,5 @@
+#include "functions/test.h"
+
+int GetGreater(int a, int b){
+    return a > b ? a : b;
+}
--- test/test.cc
+++ test/test.cc
@@ -2,11 +2,13 @@
 #include "functions/test.h"
 
 int main(int argc, char** argv){
+    std::cout <<"Performing tests..." << std::endl;
 
-    std::cout << "test" << std::endl;
-    int a = 2, b = 3;
+    int a = 2, b = 4;
     int result = GetGreater(a, b);
-    
+    if(result != b){
+        return EXIT_FAILURE;
+    }
 
     return 0;
 }
